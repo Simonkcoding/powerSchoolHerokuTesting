f04b734231e79c9168bba85d465b55393af863fd
simonkcoding <ykwok3@uncc.edu>
2018-11-27 20:26:41 -0500

    123

--- client/src/components/pages/LoginForm/LoginForm.js
+++ client/src/components/pages/LoginForm/LoginForm.js
@@ -7,6 +7,112 @@ import "./LoginForm.css";
 import Particles from "react-particles-js";
 import { Card } from "reactstrap";
 
+const particles = 
+{
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 868.0624057955
+      }
+    },
+    color: {
+      value: "#ffffff"
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#000000"
+      },
+      polygon: {
+        nb_sides: 5
+      }
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false
+      }
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false
+      }
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 6,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200
+      }
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse"
+      },
+      onclick: {
+        enable: false,
+        mode: "push"
+      },
+      resize: true
+    },
+    modes: {
+      grab: {
+        distance: 400,
+        line_linked: {
+          opacity: 1
+        }
+      },
+      bubble: {
+        distance: 400,
+        size: 40,
+        duration: 2,
+        opacity: 8,
+        speed: 3
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4
+      },
+      push: {
+        particles_nb: 4
+      },
+      remove: {
+        particles_nb: 2
+      }
+    }
+  },
+  retina_detect: true
+}
 
 export class NonUpdatingParticles extends Particles {
   shouldComponentUpdate(nextProps) {
@@ -72,111 +178,7 @@ class LoginForm extends Component {
         <div className="login">
           <div id="particles">
             <Particles
-              params={{
-                particles: {
-                  number: {
-                    value: 100,
-                    density: {
-                      enable: true,
-                      value_area: 868.0624057955
-                    }
-                  },
-                  color: {
-                    value: "#ffffff"
-                  },
-                  shape: {
-                    type: "circle",
-                    stroke: {
-                      width: 0,
-                      color: "#000000"
-                    },
-                    polygon: {
-                      nb_sides: 5
-                    }
-                  },
-                  opacity: {
-                    value: 0.5,
-                    random: false,
-                    anim: {
-                      enable: false,
-                      speed: 1,
-                      opacity_min: 0.1,
-                      sync: false
-                    }
-                  },
-                  size: {
-                    value: 3,
-                    random: true,
-                    anim: {
-                      enable: false,
-                      speed: 40,
-                      size_min: 0.1,
-                      sync: false
-                    }
-                  },
-                  line_linked: {
-                    enable: true,
-                    distance: 150,
-                    color: "#ffffff",
-                    opacity: 0.4,
-                    width: 1
-                  },
-                  move: {
-                    enable: true,
-                    speed: 6,
-                    direction: "none",
-                    random: false,
-                    straight: false,
-                    out_mode: "out",
-                    bounce: false,
-                    attract: {
-                      enable: false,
-                      rotateX: 600,
-                      rotateY: 1200
-                    }
-                  }
-                },
-                interactivity: {
-                  detect_on: "canvas",
-                  events: {
-                    onhover: {
-                      enable: true,
-                      mode: "repulse"
-                    },
-                    onclick: {
-                      enable: false,
-                      mode: "push"
-                    },
-                    resize: true
-                  },
-                  modes: {
-                    grab: {
-                      distance: 400,
-                      line_linked: {
-                        opacity: 1
-                      }
-                    },
-                    bubble: {
-                      distance: 400,
-                      size: 40,
-                      duration: 2,
-                      opacity: 8,
-                      speed: 3
-                    },
-                    repulse: {
-                      distance: 200,
-                      duration: 0.4
-                    },
-                    push: {
-                      particles_nb: 4
-                    },
-                    remove: {
-                      particles_nb: 2
-                    }
-                  }
-                },
-                retina_detect: true
-              }}
+              params={particles}
             />
           </div>
           <Card className="loginFormBuffer">
